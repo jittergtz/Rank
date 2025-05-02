22a70ebb8d6b8f56ee3542e2e206299ab0c4d623
jittergtz <sandro.gantze@icloud.com>
2025-05-02 23:45:21 +0200

    nav update

--- components/app/Navigation.tsx
+++ components/app/Navigation.tsx
@@ -8,15 +8,15 @@ function Navigation() {
     const pathname = usePathname()
 
     return (
-        <nav className='fixed bottom-0 w-full h-20'>
-            <div className='flex justify-around text-neutral-500 items-center h-full'>
-                <Link href="/" className={pathname === '/app' ? 'text-neutral-100' : ''}>
+        <nav className='fixed bottom-5 w-full '>
+            <div className='flex justify-center backdrop-blur-2xl rounded-xl  text-neutral-500 items-center h-full'>
+                <Link href="/" className={`  p-4 w-40  flex justify-center  ${pathname === '/app' ? 'text-neutral-100' : ''}`}>
                     <House size={20} />
                 </Link>
-                <Link href="/app/stats" className={pathname === '/app/stats' ? 'text-neutral-100' : ''}>
+                <Link href="/app/stats" className={` p-4 w-40  flex justify-center  ${pathname === '/app/stats' ? 'text-neutral-100' : ''}`}>
                     <ChartBar size={20} />
                 </Link>
-                <Link href="/app/profil" className={pathname === '/app/profil' ? 'text-neutral-100' : ''}>
+                <Link href="/app/profil" className={`p-4 w-40  flex justify-center ${pathname === '/app/profil' ? 'text-neutral-100' : ''}`}>
                     <Bolt size={20} />
                 </Link>
             </div>
